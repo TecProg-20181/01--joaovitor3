ef2b8752a6a479fb0fc71345093d3a9ca305ac20
joaovitor3 <joao_vitor_04@hotmail.com>
2018-03-22 12:26:06 -0300

    [CODE WITH ERROR]Trying to create min function and trying to fix errors in blur image

--- main.c
+++ main.c
@@ -21,6 +21,12 @@ int maior_inteiro(int numero_a, int numero_b) {
     return numero_b;
 }
 
+int menor_inteiro(int numero_a, int numero_b) {
+    if (numero_a > numero_b)
+        return numero_b;
+    return numero_a;
+}
+
 int verifica_pixel_igual(Pixel pixel_1, Pixel pixel_2) {
   if (pixel_1.r == pixel_2.r &&
       pixel_1.g == pixel_2.g &&
@@ -89,34 +95,6 @@ Image blur(Image img, int size) {
     }
     return img;
 }
-
-
-// void blur(unsigned int h, unsigned short int pixel[512][512][3], int T, unsigned int w) {
-//     for (unsigned int i = 0; i < h; ++i) {
-//         for (unsigned int j = 0; j < w; ++j) {
-//             Pixel media = {0, 0, 0};
-//
-//             int menor_h = (h - 1 > i + T/2) ? i + T/2 : h - 1;
-//             int min_w = (w - 1 > j + T/2) ? j + T/2 : w - 1;
-//             for(int x = (0 > i - T/2 ? 0 : i - T/2); x <= menor_h; ++x) {
-//                 for(int y = (0 > j - T/2 ? 0 : j - T/2); y <= min_w; ++y) {
-//                     media.r += pixel[x][y][0];
-//                     media.g += pixel[x][y][1];
-//                     media.b += pixel[x][y][2];
-//                 }
-//             }
-//
-//             // printf("%u", media.r)
-//             media.r /= T * T;
-//             media.g /= T * T;
-//             media.b /= T * T;
-//
-//             pixel[i][j][0] = media.r;
-//             pixel[i][j][1] = media.g;
-//             pixel[i][j][2] = media.b;
-//         }
-//     }
-// }
 
 Image rotacionar90direita(Image img) {
     Image rotacionada;
@@ -291,7 +269,6 @@ int main() {
     for(int i = 0; i < n_opcoes; ++i) {
         int opcao;
         scanf("%d", &opcao);
-        // img = menu(opcao, img);
 
         switch(opcao) {
             case 1: { // Escala de Cinza
@@ -310,7 +287,6 @@ int main() {
             }
             case 4: { // Rotacao
                 img = rotaciona(img);
-                break;
             }
             case 5: { // Espelhamento
                 img = espelhamento(img);
